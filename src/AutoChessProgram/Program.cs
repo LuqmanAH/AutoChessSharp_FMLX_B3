b660aa26c31977df5f533e4f46199e79603d9671
luqmanAH <luqman.alhelmy@gmail.com>
2023-07-28 22:35:31 +0700

    Continued testing GR methods - TODO: test piece and position - TODO: Clarify piece positioning - TODO: Defining store system

--- src/AutoChessProgram/Program.cs
+++ src/AutoChessProgram/Program.cs
@@ -11,7 +11,8 @@ class Program
         // Console.WriteLine("\n----------\n");
         // PlayerInfoTest();
         // Console.WriteLine("\n----------\n");
-        PlayerDictTest();
+        // PlayerDictTest();
+        GetHPTest();
 
         Console.Read();
     }
@@ -21,7 +22,7 @@ class Program
         //* Player generation case 1 test: null default ctor
         Player player_1 = new Player();
         Player player_2 = new Player("Lumine");
-        Player player_3 = new Player();
+        Player player_3 = new Player("Pestering Fauna");
         Helper.ProgramPrinter(player_1.GetName());
 
         //* Player generation case 2 test: ctor overload pass string as name
@@ -34,6 +35,12 @@ class Program
         //* Player_2 allows name reset
         player_2.SetPlayerName("Jean Dancho");
         Helper.ProgramPrinter(player_2.GetName());
+
+        //* Setter test on overwriting identic playerName
+        Helper.ProgramPrinter(player_3.GetName());
+        bool setInfo = player_3.SetPlayerName("Pestering Fauna");
+        Helper.ProgramPrinter(player_3.GetName());
+        Helper.ProgramPrinter($"Operation resulted in: {setInfo}");
 
         Helper.ProgramPrinter(player_1.GetID().ToString());
         Helper.ProgramPrinter(player_2.GetID().ToString());
@@ -76,6 +83,37 @@ class Program
             Helper.ProgramPrinter($"{player.GetName()} associated with {info}");
 
         }
+    }
+
+    static void GetHPTest()
+    {
+        GameRunner autoChess = new GameRunner();
+        Player player1= new Player("Baal");
+        Player player2= new Player("Buer");
+        Player player3= new Player("Barbatos");
+
+        autoChess.AddPlayer(player1);
+        autoChess.AddPlayer(player2);
+
+        Dictionary<IPlayer, int> playerHealthData = autoChess.ShowPlayerHealth();
+
+        foreach (var kvp in playerHealthData)
+        {
+            Helper.ProgramPrinter($"Player {kvp.Key.GetName()} has HP of {kvp.Value}");
+        }
+
+        int player1Health = autoChess.ShowPlayerHealth(player1);
+        Helper.ProgramPrinter($"{player1.GetName()} currently has HP of {player1Health}");
+
+        try
+        {
+            int player3Health = autoChess.ShowPlayerHealth(player3);
+        }
+        catch(KeyNotFoundException ex)
+        {
+            Console.WriteLine($"{player3.GetName()} {ex.Message}");
+        }
+
     }
 }
 
--- src/AutoChessSharp.Core/GameRunner.cs
+++ src/AutoChessSharp.Core/GameRunner.cs
@@ -10,16 +10,72 @@ public class GameRunner
 
     public bool AddPlayer(IPlayer player)
     {
-        if (playerDetail.ContainsKey(player))
-        {
-            return false;
-        }
-        playerDetail.Add(player, new PlayerInfo());
-        return true;
+        bool addSuccess = playerDetail.TryAdd(player, new PlayerInfo());
+        return addSuccess;
     }
 
     public Dictionary<IPlayer, PlayerInfo> GetAllPlayers()
     {
         return playerDetail;
+    }
+
+    public Dictionary<IPlayer, int> ShowPlayerHealth()
+    {
+        if (playerDetail == null)
+        {
+            throw new NullReferenceException(message : "No Added Players Yet!");
+        }
+
+        Dictionary<IPlayer, int> PlayersHealth = new();
+        foreach (var details in playerDetail)
+        {
+            IPlayer playerInGame = details.Key;
+            int playerHealthPoint = details.Value.GetHealth();
+
+            PlayersHealth.Add(playerInGame, playerHealthPoint);
+        }
+        return PlayersHealth;
+    }
+    public int ShowPlayerHealth(IPlayer player)
+    {
+        if (playerDetail == null)
+        {
+            throw new NullReferenceException(message : "No Added Players Yet!");
+        }
+
+        if (!playerDetail.ContainsKey(player))
+        {
+            throw new KeyNotFoundException(message : "Player Not Found");
+        }
+
+        int playerHealth = 0;
+        foreach (var details in playerDetail)
+        {
+            if (details.Key.GetID() == player.GetID())
+            {
+                playerHealth += details.Value.GetHealth();
+            }
+        }
+        return playerHealth;
+    }
+
+    public List<IPlayer> GetAlivePlayers()
+    {
+        if (playerDetail == null)
+        {
+            throw new NullReferenceException(message : "No Added Player Yet!");
+        }
+
+        List<IPlayer> alivePlayers = new();
+        foreach (var details in playerDetail)
+        {
+            int playerHealthPoint = details.Value.GetHealth();
+
+            if (playerHealthPoint > 0)
+            {
+                alivePlayers.Add(details.Key);
+            }
+        }
+        return alivePlayers;
     }
 }
